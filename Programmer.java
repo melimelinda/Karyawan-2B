a7228dc84a2b2992580b6500dbca754bf9f738c4
Meli Melinda <melinawada29@gmail.com>
2018-10-12 09:33:56 +0700

    ref4

--- Programmer.java
+++ Programmer.java
@@ -1,7 +1,7 @@
 public class Programmer extends Karyawan{
 	public void setGaji(int uang){
 		if(uang<=90){
-			System.out.println("Gaji Anda bulan ini adalah "+gaji);
+			System.out.println("Gaji Anda bulan ini adalah "+uang);
 		}else{
 			System.out.println("maaf gaji anda melebihi standarisasi perusahaan");
 		}
